7d857b1938912f4cd7d6a6f5ce2b5d9fe16760c9
pritam biswas <pritamb@creditnirvana.ai>
2023-01-16 02:47:14 +0530

    tailwind added.

---
+++ src/MainApp/MainApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer/Footer'
+import Header from './Header/Header';
+
+const  MainApp:React.FC = ()=>{
+  return (
+    <div className='md:container md:mx-auto h-full bg-white  border-white '>
+          hsdhsd
+    </div>
+  )
+}
+
+export default MainApp
